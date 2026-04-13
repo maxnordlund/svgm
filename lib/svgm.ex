6b15a2102c7b451c361b6654070d788077243a3c
Max Nordlund <max.nordlund@gmail.com>
2026-04-13 16:17:53 +0200

    Initial commit

---
+++ lib/svgm.ex
@@ -0,0 +1,18 @@
+defmodule Svgm do
+  @moduledoc """
+  Documentation for `Svgm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Svgm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/svgm_test.exs
@@ -0,0 +1,8 @@
+defmodule SvgmTest do
+  use ExUnit.Case
+  doctest Svgm
+
+  test "greets the world" do
+    assert Svgm.hello() == :world
+  end
+end
